f2d0c9c8f84499060b49ff9be51198ea884fe11f
larissa <souzalarissa383@gmail.com>
2020-03-30 20:55:03 -0300

    finalizado

---
+++ main.js
@@ -0,0 +1,11 @@
+axios.get('https://api.gi3333thub.com/users/souzalarissa383')
+
+
+minhaPromise()
+.then(function(response){
+    console.log(response);
+});
+
+.catch(function(error) {
+   console.warn(error);
+});
